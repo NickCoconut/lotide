a8a29e8b5c4c35f5e762f54a367cd449cd737180
NickCoconut <gelaxyy@gmail.com>
2021-11-29 15:55:29 +0000

    modified eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (JSON.stringify(array1) === JSON.stringify(array2)) {
@@ -13,4 +7,5 @@ const eqArrays = function(array1, array2) {
   return false
 }
 }
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
+
+module.exports = eqArrays;
